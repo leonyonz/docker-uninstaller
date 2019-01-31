09c17d163d4544f219edba72324945d7399f73e3
leonyonz <42370275+leonyonz@users.noreply.github.com>
2019-01-31 10:51:05 +0700

    Update file .sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -4,3 +4,6 @@ sudo apt -y remove docker docker-engine docker.io containerd runc
 ls /var/lib/ | grep doc
 rm -rf /var/lib/docker
 ls /var/lib/ | grep doc
+ls /etc/ | grep docker
+rm -rf /etc/docker
+ls /etc/ |grep docker
