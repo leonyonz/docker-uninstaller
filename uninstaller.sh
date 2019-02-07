3ea5e7164b6dc1f9a90c3667155ca505779a629e
leonyonz <42370275+leonyonz@users.noreply.github.com>
2019-02-08 01:30:58 +0700

    edit make perfect

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-sudo apt -y remove docker docker-engine docker.io containerd runc
+sudo apt -y remove docker docker-engine docker.io containerd runc lxc-docker-1.9.1
+sudo apt -y autoclean lxc-docker-1.9.1
 ls /var/lib/ | grep doc
 rm -rf /var/lib/docker
 ls /var/lib/ | grep doc
 ls /etc/ | grep docker
 rm -rf /etc/docker
 ls /etc/ |grep docker
+umount /var/lib/docker/aufs
+rm -fr /var/lib/docker
