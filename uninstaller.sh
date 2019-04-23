7b267ff2c6ea15eb636029d12ac114a6679a7522
leonyonz <42370275+leonyonz@users.noreply.github.com>
2019-04-23 12:16:25 +0700

    Fix Script

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-sudo apt -y remove docker docker-engine docker.io containerd runc lxc-docker-1.9.1
-sudo apt -y autoclean lxc-docker-1.9.1
+sudo apt -y remove docker docker-ce docker-engine docker.io containerd runc
+sudo apt -y autoclean docker-ce docker.io docker
+sudo apt -y purge docker-ce docker.io docker
+sudo apt -y autoremove
 ls /var/lib/ | grep doc
 rm -rf /var/lib/docker
 ls /var/lib/ | grep doc
@@ -9,4 +11,4 @@ ls /etc/ | grep docker
 rm -rf /etc/docker
 ls /etc/ |grep docker
 umount /var/lib/docker/aufs
-rm -fr /var/lib/docker
+rm -rf /var/lib/docker
