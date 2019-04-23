b0674272b7abf86ee9bb18982579367f1419b9f5
leonyonz <42370275+leonyonz@users.noreply.github.com>
2019-04-23 12:29:49 +0700

    update

--- uninstaller.sh
+++ uninstaller.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+##Unintall Package
 sudo apt -y remove docker docker-ce docker-engine docker.io containerd runc
 sudo apt -y autoclean docker-ce docker.io docker
 sudo apt -y purge docker-ce docker.io docker
 sudo apt -y autoremove
+
+##Remove docker dir & files
 ls /var/lib/ | grep doc
 rm -rf /var/lib/docker
 ls /var/lib/ | grep doc
@@ -12,6 +15,9 @@ rm -rf /etc/docker
 ls /var/run/ | grep docker
 rm -rf /etc/docker
 ls /etc/ |grep docker
+
+##unmount volume driver
 umount /var/lib/docker/aufs
 rm -rf /var/lib/docker
+echo ""
 echo "DONE ! Make sure docker is uninstalled"
