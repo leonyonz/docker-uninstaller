4d40c41ffb4ffdfbd964dcd3073a8600cd4c9f37
sidarmawan <sidarmawan8@gmail.com>
2019-04-30 11:44:35 +0700

    update snap remover

--- uninstaller.sh
+++ uninstaller.sh
@@ -5,6 +5,7 @@ sudo apt -y remove docker docker-ce docker-engine docker.io containerd runc
 sudo apt -y autoclean docker-ce docker.io docker
 sudo apt -y purge docker-ce docker.io docker
 sudo apt -y autoremove
+sudo snap remove docker
 
 ##Remove docker dir & files
 ls /var/lib/ | grep doc
@@ -15,6 +16,8 @@ rm -rf /etc/docker
 ls /var/run/ | grep docker
 rm -rf /var/run/docker
 rm -rf /var/run/docker.sock
+ls /snap | grep docker
+rm -rf /snap/docker/
 
 ##unmount volume driver
 umount /var/lib/docker/aufs
